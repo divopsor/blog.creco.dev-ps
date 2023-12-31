6e8fd002d17a19db1224714da93f52355479a74f
divopsor <divopsor@gmail.com>
2023-12-31 22:21:52 +0900

    fix: date format

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,14 @@
+function formattedDate(date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDay() + 1;
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초`;
+}
+
 export function parsePs(item?: any) {
   if (item == null) {
     return {};
@@ -7,8 +18,7 @@ export function parsePs(item?: any) {
 
   const [category, title, ...body] = details?.body?.contents?.trim?.().split?.('\n') ?? [];
   const timestamp = details?.body?.updatedAt ?? details?.body?.createdAt;
-  const koreanGMT = 9 * 3600_000;
-  const date = new Date(timestamp + koreanGMT).toLocaleString('ko-KR', { timeZone: 'UTC' });
+  const date = formattedDate(new Date(timestamp));
 
   return {
     id: details.id,
