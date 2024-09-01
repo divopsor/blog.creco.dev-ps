984d5bb57c72da3957763a4b531a47cfcca6a022
divopsor <divopsor@gmail.com>
2024-09-01 22:21:07 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,17 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <CrecoApp.Heads />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4M82715ZRX"></script>
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4M82715ZRX');
+            `
+          }
+        }/>
       </Head>
       <CrecoApp queryClient={queryClient}>
         <Component {...pageProps} />
